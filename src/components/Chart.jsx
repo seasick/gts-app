92e2b452cc36d503dde9374d11a82b3a4a186442
Clemens Solar <solar.c@gmx.net>
2022-01-06 23:51:40 +0100

    feat: change sort order of lines
    with this change the gts is displayed above the temperature in the tooltip

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -42,19 +42,18 @@ export default function Chart({mode, station}) {
       }} />
       <Line
         type="monotone"
+        dataKey="cumulative"
+        name="Grünlandtemperatursumme"
+        stroke="#82ca9d"
+        yAxisId={0}
+      />
+      <Line
+        type="monotone"
         dataKey="temperatur"
         name="Temperatur"
         stroke="#8884d8"
         yAxisId={0}
-        />
-
-    <Line
-      type="monotone"
-      dataKey="cumulative"
-      name="Grünlandtemperatursumme"
-      stroke="#82ca9d"
-      yAxisId={0}
-    />
+      />
     </LineChart>
   </ResponsiveContainer>;
 }
