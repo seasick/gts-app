dfa7dde5dce7916966385a948da61dc0efd173af
Clemens Solar <solar.c@gmx.net>
2022-01-06 23:38:34 +0100

    feat: add station and mode to info text

--- src/components/Gst.jsx
+++ src/components/Gst.jsx
@@ -1,20 +1,34 @@
 import {Alert, AlertTitle} from '@mui/material';
 import useData from '../hooks/useData';
 import {calculateGstForStation, calculateWeightedGstForStation} from '../gst';
+import useStations from '../hooks/useStations';
 
 
 export default function Gst({mode, station}) {
   const [data, isLoading] = useData();
+  const [stations] = useStations();
+  let stationName;
   let value;
+  let label;
 
   if (isLoading) {
     return <div>Loading</div>;
   }
 
+  // Determine current station name
+  for (let i = 0; i < stations.length; ++i) {
+    if (stations[i].value === station) {
+      stationName = stations[i].label;
+      break;
+    }
+  }
+
   if (mode === 'gts') {
     value = calculateGstForStation(data, station);
+    label = 'Grünlandtemperatursumme';
   } else if (mode === 'weighted_gts') {
     value = calculateWeightedGstForStation(data, station);
+    label = 'gewichtete Grünlandtemperatursumme';
   } else {
     value = false;
   }
@@ -28,6 +42,6 @@ export default function Gst({mode, station}) {
 
   return <Alert severity="info">
     <AlertTitle>Info</AlertTitle>
-    Aktuelle Grünlandtemperatursumme ist <b>{Math.round(value * 100) / 100}</b>
+    Aktuelle {label} in <b>{stationName}</b> ist <b>{Math.round(value * 100) / 100}</b>
   </Alert>;
 }
