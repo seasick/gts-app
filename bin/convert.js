e2cf2b29b4b6aca5b14b04585402c7a73535a6c6
Clemens Solar <solar.c@gmx.net>
2022-01-07 22:30:16 +0100

    feat: add forcast data fetching

--- bin/convert.js
+++ bin/convert.js
@@ -3,9 +3,14 @@ const {writeFile} = require('fs/promises');
 const path = require('path');
 const aggregateZAMG = require('../lib/zamg');
 const aggregateMeteoblue = require('../lib/meteoblue');
+const openweathermap = require('../lib/openweathermap');
 
 
 (async () => {
+  const openweatherApiKey = process.argv[2];
+  const lat = process.argv[3];
+  const lon = process.argv[4];
+
   // Add ZAMG data
   console.log('Aggregate ZAMG data');
   let data = await aggregateZAMG();
@@ -14,9 +19,32 @@ const aggregateMeteoblue = require('../lib/meteoblue');
   console.log('Aggregate Meteoblue data');
   data = data.concat(await aggregateMeteoblue());
 
+  // Get forcast
+  let forcast = await openweathermap(openweatherApiKey, lat, lon);
+  forcast = forcast.daily.map((cast) => {
+    const date = new Date(cast.dt * 1000);
+
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const dateString = `${year}-${month}-${day}`;
+
+    return {
+      date: dateString,
+      stationName: 'Zistersdorf',
+      stationId: 'meteoblue-zistersdorf',
+      meanTemperature: cast.temp.day,
+      medianTemperature: cast.temp.day
+    };
+  });
+
   await writeFile(
     path.resolve(__dirname, '../public/data/aggregated.json'),
     JSON.stringify(data)
   );
+  await writeFile(
+    path.resolve(__dirname, '../public/data/forcast.json'),
+    JSON.stringify(forcast)
+  )
 
 })();
